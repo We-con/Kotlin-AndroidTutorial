ba1d0b58e5744d63238e6f1b4e1290471a178c48
octave08 <octave08b@gmail.com>
2017-09-02 13:50:47 +0900

    Add a example for Fab

---
+++ FloatingActionButton/app/src/main/java/com/wecon/mangob/floatingactionbutton/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.wecon.mangob.floatingactionbutton
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var isFabOpen = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener {
+            animateFab()
+        }
+        fab_action_call.setOnClickListener {
+            Toast.makeText(this, "Hello call", Toast.LENGTH_SHORT).show()
+            animateFab()
+        }
+        fab_action_email.setOnClickListener {
+            Toast.makeText(this, "Hello email", Toast.LENGTH_SHORT).show()
+            animateFab()
+        }
+    }
+
+    private fun animateFab() {
+        when(isFabOpen) {
+            true -> closeFab()
+            false -> openFab()
+        }
+        isFabOpen = !isFabOpen
+    }
+
+    private fun openFab() {
+        AnimationUtils.loadAnimation(applicationContext, R.anim.rotate_forward)?.let {
+            fab.startAnimation(it)
+        }
+
+        AnimationUtils.loadAnimation(applicationContext, R.anim.fab_open)?.let {
+            fab_action_call.startAnimation(it)
+            text_fab_call.startAnimation(it)
+            fab_action_email.startAnimation(it)
+            text_fab_mail.startAnimation(it)
+        }
+        fab_action_call.isClickable = true
+        fab_action_email.isClickable = true
+    }
+
+    private fun closeFab() {
+        AnimationUtils.loadAnimation(applicationContext, R.anim.rotate_backword)?.let {
+            fab.startAnimation(it)
+        }
+
+        AnimationUtils.loadAnimation(applicationContext, R.anim.fab_close)?.let {
+            fab_action_call.startAnimation(it)
+            text_fab_call.startAnimation(it)
+            fab_action_email.startAnimation(it)
+            text_fab_mail.startAnimation(it)
+        }
+        fab_action_call.isClickable = false
+        fab_action_email.isClickable = false
+    }
+
+}
